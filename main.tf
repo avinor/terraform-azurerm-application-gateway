fc16f3bf7d3ec43f70ad269b5cae7f416a1546dd
Øyvind Strømmen <insula.ventus@gmail.com>
2021-10-29 13:25:53 +0200

    Use azurerm v2.83.0

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.53.0"
+      version = "~> 2.83.0"
     }
   }
 }
 
-provider azurerm {
+provider "azurerm" {
   features {}
 }
 
